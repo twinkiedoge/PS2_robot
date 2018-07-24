275cfc37946244768c1914427308b64026cefc2f
twinkiedoge <abobroff8@gmail.com>
2018-07-24 11:45:35 -0700

    crash avoidance

--- PS2_robot.ino
+++ PS2_robot.ino
@@ -19,15 +19,33 @@ byte type = 0;
 byte vibrate = 0;
 PS2X ps2x;
 
-
 //--------------------------stuff that has to do with motor setup--------------------------------
 
 int motorspeed = 0;
 float sensitivity = 0.8;
+float spd = 0.5;
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *rightmotor = AFMS.getMotor(3);
 Adafruit_DCMotor *leftmotor = AFMS.getMotor(4);
+//--------------------------stuff that has to do with ultrasonic setup--------------------------------
+const int trigpin = 8;
+const int echopin = 9;
+long duration;
+int distance;
 
+
+int readdistance(){
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, LOW);
+
+  duration = pulseIn(echopin, HIGH);
+  distance = duration*(0.034/2);
+  return distance;
+}
 
 void setup() {
   Serial.begin(57600);
@@ -42,6 +60,10 @@ void setup() {
   leftmotor->run(FORWARD);
   rightmotor->run(RELEASE);
   leftmotor->run(RELEASE);
+
+  //ultrasonic
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
 
   //--------------------------from PS2X library example Bill Porter?--------------------------------------------------------------
 error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
@@ -92,29 +114,55 @@ error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumbl
 }
 
 void loop() {
-ps2x.read_gamepad(false, vibrate); 
+ps2x.read_gamepad(false, vibrate);
+rightmotor->run(FORWARD);
+leftmotor->run(FORWARD);
+int dist = readdistance();
+if(dist>25){
+  if(ps2x.Button(PSB_R1)) {
     rightmotor->run(FORWARD);
     leftmotor->run(FORWARD);
-  if(ps2x.Button(PSB_R1)) {
     int rightstickx = ps2x.Analog(PSS_RX);
     if(rightstickx>134){
       int rspeed = (rightstickx*sensitivity);
       int lspeed = 255-rspeed;
-      rightmotor->setSpeed(rspeed);
-      leftmotor->setSpeed(lspeed);
+      rightmotor->setSpeed(rspeed*spd);
+      leftmotor->setSpeed(lspeed*spd);
+    
     }else if(rightstickx<120){
+      
       int lspeed = ((rightstickx*-1)+255)*sensitivity;
       int rspeed = 255-lspeed;
-      rightmotor->setSpeed(rspeed);
-      leftmotor->setSpeed(lspeed);
+      rightmotor->setSpeed(rspeed*spd);
+      leftmotor->setSpeed(lspeed*spd);
+    
     }else{
-      rightmotor->setSpeed(200);
-      leftmotor->setSpeed(200);
-    }
+      
+      rightmotor->setSpeed(200*spd);
+      leftmotor->setSpeed(200*spd);
+     }
    }
-   if (ps2x.NewButtonState(PSB_CROSS)){
+  if (ps2x.NewButtonState(PSB_CROSS)){
       rightmotor->setSpeed(0);
       leftmotor->setSpeed(0);
-      
    }
+  
+}else{
+  rightmotor->setSpeed(150*spd);
+  leftmotor->setSpeed(150*spd);
+  delay(200);
+  rightmotor->setSpeed(100*spd);
+  leftmotor->setSpeed(100*spd);
+  delay(200);
+  rightmotor->setSpeed(0);
+  leftmotor->setSpeed(0);
+  rightmotor->run(BACKWARD);
+  leftmotor->run(BACKWARD);
+  rightmotor->setSpeed(100*spd);
+  leftmotor->setSpeed(100*spd);
+  delay(1000);
+  
+  }
 }
+
+
